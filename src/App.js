33be3bbacb9738a9db979c9a82d878043de470b6
hafsahussein <49996540+hafsahussein@users.noreply.github.com>
2020-11-21 09:51:34 +0300

    created app bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import "./styles.css";
-
+import AppBar from '@material-ui/core/AppBar'
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar'
+import {makeStyles} from '@material-ui/core/styles'
+const useStyles = makeStyles((theme)=>({
+appBar:{
+backgroundColor:'#fff'
+}
+}))
 export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div className="App" >
+      <AppBar position="static" className ={classes.appBar}>
+        <Toolbar>
+          <Typography variant ='h6' color ="primary">
+                Blog Website
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {ThemeProvider} from '@material-ui/core/styles'
+import {Theme} from './theme'
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
+<ThemeProvider theme = {Theme}>
     <App />
+</ThemeProvider>
   </React.StrictMode>,
   rootElement
 );
